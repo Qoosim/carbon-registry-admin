6fa029a0c25542c4dd08bbf9f2c263ae320c8f6d
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-04-27 09:57:56 +0100

    install necessary libraries

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      telex: ['Telex', 'sans-serif'],
+      rubik: ['Rubik', 'sans-serif'],
+      poppins: ['Poppins', 'sans-serif'],
+      notoSans: ['"Noto Sans Display"', 'sans-serif']
+    },
+    extend: {
+      width: {},
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
+export default config;
