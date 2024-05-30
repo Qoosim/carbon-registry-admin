d3e91c854a7753a1ac8c5f77b1cd3fb3e282cce8
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-05-30 21:07:04 +0100

    implement verify token logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,28 @@
-import { NextRequest, NextResponse } from "next/server"
 
-export const middleware = (request: NextRequest) => {
-  
- 
+/* eslint-disable consistent-return */
+import { NextResponse, NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const unprotectedPaths = ["/login", "/signup"];
+
+  const isPublicPath = unprotectedPaths.includes(path);
+
+  const user = request.cookies.get("userData")?.value || "";
+
+  if (isPublicPath && user) {
+    return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
+  }
+
+  if (!isPublicPath && !user && path !== "/login") {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: []
-}
+  matcher: ["/", "/login", "/signup", "/dashboard"],
+};
+
